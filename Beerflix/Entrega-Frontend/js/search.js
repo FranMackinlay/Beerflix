b794df8771a0abb0b426bcb029f32de1888a6612
Francisco Mackinlay <franmackinlay@gmail.com>
2020-01-22 22:56:31 -0300

    date

--- Beerflix/Entrega-Frontend/js/search.js
+++ Beerflix/Entrega-Frontend/js/search.js
@@ -7,7 +7,7 @@ export const STORAGE_TYPE = 'lStorage';
 const { setItem, getItem } = storage('lStorage')
 
 const searchForm = document.querySelector('#search-form');
-const searchInput = document.querySelector('.input.search');
+const searchInput = document.querySelector('.input-search');
 
 searchInput.value = getItem(INPUT_STORAGE_ID);
 
